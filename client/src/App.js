cb0706b971419b16e366683821eaefe9253c6ed0
Scott Donnan <scott.donnan@me.com>
2021-11-16 16:37:32 -0600

    added signin and signup components and create user route in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, Button} from 'react-bootstrap';
+
 // import Button from 'react-bootstrap/Button';
 
 function App() {
+  const [login, setLogin] = useState(false)
+
   return (
-    <div className="App">
-      <button>Test</button>
+    <div>
+      {login ? <Signin setLogin={setLogin}/> : <Signup setLogin={setLogin}/>}
     </div>
   );
 }
--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -1,11 +1,38 @@
+import {Button, Form, Container} from 'react-bootstrap'
 
-function Signin() {
+function Signin({setLogin}) {
+    function handleClick(event) {
+        event.preventDefault()
+        setLogin(false)
+    }
+    
     return(
-        <div>
-            <form>
-                
-            </form>
-        </div>
+        <Container className="w-50 p-3 mt-5">
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            
+            {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group> */}
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            <Button variant="secondary" type="submit" onClick={handleClick}>
+                Sign Up
+            </Button>
+        </Form>
+        </Container>
     )
 }
 
