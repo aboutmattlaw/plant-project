193dffea5daa50285bb4cc956ca291457237d714
Scott Donnan <scott.donnan@me.com>
2021-11-18 12:15:05 -0600

    added gardens as a route in the navbar / a few more css related updates

--- client/src/components/AuthenticatedApp.jsx
+++ client/src/components/AuthenticatedApp.jsx
@@ -3,7 +3,6 @@ import BigGarden from "./BigGarden"
 function AuthenticatedApp({setCurrentUser, currentUser, setCurrentUserGardens, currentUserGardens}) {
     return (
         <div>
-            Authenticated App 
             <BigGarden setCurrentUser={setCurrentUser} currentUser={currentUser} setCurrentUserGardens={setCurrentUserGardens} currentUserGardens={currentUserGardens} />
         </div>
     )
